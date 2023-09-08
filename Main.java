0ce9c7c0a112df13946058901596e28fa4500697
joseneto99 <111126206+joseneto99@users.noreply.github.com>
2023-09-08 22:28:37 +0000

    1

--- Main.java
+++ Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        Assalariado A = new Assalariado("João", "Antunes", 256352);
+        Assalariado A = new Assalariado(" João", "Antunes", 256352);
         A.vencimento(5000);
-        System.out.println(A);
-        
+        System.out.println("Assalariado: " + A);
+         
 
-        Comissionado B = new Comissionado("Marcos", "Castro", 56864223);
+        Comissionado B = new Comissionado(" Marcos", "Castro", 56864223);
         B.vencimento(1000, 5);
-        System.out.println(B);
+        System.out.println("Comissionado: "+ B);
         
 
-        Horista C = new Horista("Ana", "Maria", 96482432);
+        Horista C = new Horista(" Ana", "Maria", 96482432);
         C.vencimento(10, 50);
-        System.out.println(C);
+        System.out.println("Horista: " + C);
      
 
         
